f9afed9f5b97b54d9a5a6d50e42fe6aa27fa0a38
mungocactus <paul.hess@blueyonder.co.uk>
2020-05-17 19:04:16 +0100

    adds validation

--- src/App.js
+++ src/App.js
@@ -2,34 +2,56 @@ import React from 'react';
 import './styles/styles.scss';
 import logo from './green-logo.svg';
 
-function App() {
-  return (
-    <div className="login">
-      <img src={logo} alt='Green Energy logo' />
-      <form>
-        <h1>Example login screen</h1>
-        <p>Getting started with Green</p>
-        <div className="input-container">
-          <label for="email">Email Address</label>
-          <input
-            class=""
+class App extends React.Component {
+  constructor() {
+    super();
+    this.validateForm = this.validateForm.bind(this);
+  }
+
+  emailInput = React.createRef();
+
+  validateForm(event) {
+    event.preventDefault();
+    const emailInput = this.emailInput.current.value;
+    const emailRegex = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;
+    const signInBtn = document.querySelector('#signin');
+
+    if (emailInput.match(emailRegex) !== null) {
+      signInBtn.disabled = false;
+    } else {
+      signInBtn.disabled = true;
+    }
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <img src={logo} alt='Green Energy logo' />
+        <form>
+          <h1>Example login screen</h1>
+          <p>Getting started with Green</p>
+          <div className="input-container">
+            <label htmlFor="email">Email Address</label>
+            <input
             type="email"
             id="email"
-            pattern=".+@globex.com"
-            size="30"
+            ref={this.emailInput}
+            onChange={this.validateForm}
             required
-          />
-        </div>
-        <div className="checkbox-container">
-          <label>Remember this device</label>
-          <input
+            />
+          </div>
+          <div className="checkbox-container">
+            <label htmlFor="remember">Remember this device</label>
+            <input
             type="checkbox"
-          />
-        </div>
-        <button>Sign In</button>
-      </form>
-    </div>
-  );
+            id="remember"
+            />
+            </div>
+          <button type="submit" id="signin" disabled>Sign In</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
